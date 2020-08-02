9e2b822736279f998aa0d2493f51d6602c65e6da
unknown <rezarb88@gmail.com>
2020-08-02 11:24:02 +0700

    persisting old from data

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,5 +59,6 @@ $container['AuthController'] = function ($container) {
 };
 
 $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
+$app->add(new \App\Middleware\OldInputMiddleware($container));
 
 require __DIR__ . '/../app/routes.php';
