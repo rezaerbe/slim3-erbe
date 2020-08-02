dd91110917e516e95360ecf7003b1eb0ff502d44
unknown <rezarb88@gmail.com>
2020-08-02 16:05:29 +0700

    guest middleware

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,18 @@
 <?php
 
 use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 
 $app->get('/', 'HomeController:index')->setName('home');
 
-$app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
-$app->post('/auth/signup', 'AuthController:postSignUp');
+$app->group('', function () {
 
-$app->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
-$app->post('/auth/signin', 'AuthController:postSignIn');
+    $this->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
+    $this->post('/auth/signup', 'AuthController:postSignUp');
+
+    $this->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
+    $this->post('/auth/signin', 'AuthController:postSignIn');
+})->add(new GuestMiddleware($container));
 
 $app->group('', function () {
 
