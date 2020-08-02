a585a87f8fa2a4bd353ba5d387a35907d0f21b8d
unknown <rezarb88@gmail.com>
2020-08-02 15:10:40 +0700

    outputting flash messages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -58,6 +58,8 @@ $container['view'] = function ($container) {
         'user' => $container->auth->user(),
     ]);
 
+    $view->getEnvironment()->addGlobal('flash', $container->flash);
+
     return $view;
 };
 
