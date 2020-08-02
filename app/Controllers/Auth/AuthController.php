9afa229d86988a1c1d17029dc434bb671960cf16
unknown <rezarb88@gmail.com>
2020-08-02 12:14:29 +0700

    getting ready to sign in

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -8,9 +8,18 @@ use Respect\Validation\Validator as v;
 
 class AuthController extends Controller {
 
+    public function getSignIn($request, $response) {
+
+        return $this->view->render($response, 'auth/signin.twig');
+    }
+
     public function getSignUp($request, $response) {
 
         return $this->view->render($response, 'auth/signup.twig');
+    }
+
+    public function postSignIn($request, $response) {
+        
     }
 
     public function postSignUp($request, $response) {
